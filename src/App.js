d32892258eda859886bc55debfc405590686110a
faha1999 <kafaha1999@gmail.com>
2022-04-23 01:06:56 +0600

    added map

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from 'react';
+import Map from './components/Map';
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div>
+      <Map />
+    </div>
+  );
 }
 
 export default App;
